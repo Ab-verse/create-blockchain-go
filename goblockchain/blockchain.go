dafe826d0c2fe31df0c678ce9b84b5be95b82691
Ab-verse <abhisheksckm17@gmail.com>
2022-12-09 10:33:55 +0530

    add new block function

--- goblockchain/blockchain.go
+++ goblockchain/blockchain.go
@@ -2,8 +2,37 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"time"
 )
 
+type Block struct {
+	nonce int 
+	previousHash string
+	timestamp int64
+	transactions []string
+}
+
+func NewBlock(nonce int, previousHash string) *Block{
+	b := new(Block)
+	b.nonce = nonce
+	b.previousHash = previousHash
+	b.timestamp = time.Now().UnixNano()
+	return b
+}
+
+func (b *Block) Print() {
+	fmt.Println("nonse            ", b.nonce)
+	fmt.Println("previous_hash    ", b.previousHash)
+	fmt.Println("timestamp        ", b.timestamp)
+	fmt.Println("transactions     ", b.transactions)	
+}
+
+
+func init() {
+	log.SetPrefix("Blockchain: ")
+}
 func main() {
-	fmt.Println("test")
+	b := NewBlock(0, "init hash")
+	b.Print()
 }
